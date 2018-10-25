e4793adc80be94218436109373ce0971a9a92526
svg153 <svg153@gmail.com>
2018-10-25 06:29:40 +0000

    New aliases

--- run.sh
+++ run.sh
@@ -283,11 +283,3 @@ sudo apt clean
 #
 # CLEAN
 #
-
-
-#
-# Thanks:
-#    https://linuxpanda.wordpress.com/2016/12/31/things-to-do-after-installing-debian-stretch/
-#    https://www.youtube.com/watch?v=BWBHJmAmZgk
-#    https://www.youtube.com/watch?v=c60x3nd7cag
-#    https://www.youtube.com/watch?v=GR2y0xOIIdI
