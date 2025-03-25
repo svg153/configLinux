c99f28f2d47521edc9c954dc46ab222a60d709c6
Sergio Valverde <svg153@gmail.com>
2025-03-25 00:32:58 +0000

    feat: enhance kubectl aliases and add KUBECONFIG export

--- .bash_aliases.d/kubectl.sh
+++ .bash_aliases.d/kubectl.sh
@@ -1,4 +1,13 @@
 alias k="kubectl"
 complete -F __start_kubectl k
 
+export KUBECONFIG=$(find -L ~/.kube/configs -type f | sed ':a;N;s/\n/:/;ba')
+
 alias minikubectl="minikube kubectl --"
+
+alias kx='kubectx'
+
+#
+# tools
+#
+alias k9='k9s'
