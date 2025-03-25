01ba034963e8d9034621acd0cde244ca038cc4de
Sergio Valverde <svg153@gmail.com>
2025-03-25 03:32:24 +0100

    feat: add autocomplete for terraform command in terraform.sh

--- .rc.d/terraform.sh
+++ .rc.d/terraform.sh
@@ -1,2 +1,5 @@
 # tfenv
 export PATH="$HOME/.tfenv/bin:$PATH"
+
+complete -C $HOME/.tfenv/versions/1.11.2/terraform terraform
+# TODO: check if change the tfversion if change the tfenv version, complete -C $HOME/.tfenv/versions/$(terraform version | cut -d'v' -f2 | head -n1)/terraform terraform
