812a532260c138c16d219c7713965d654c3ef2d1
Sergio Valverde <svg153@gmail.com>
2025-04-10 17:50:07 +0000

    feat: add function to create a branch from a Jira issue

--- .bash_aliases.d/jira.sh
+++ .bash_aliases.d/jira.sh
@@ -45,3 +45,40 @@
 #     git checkout -b "$branch_name"
 #   fi
 # }
+
+function create-branch() {
+    set -x
+
+  local jq_template query branch_name
+
+  jq_template='"'\
+'\(.key). \(.fields.summary)'\
+'\t'\
+'Reporter: \(.fields.reporter.displayName)\n'\
+'Created: \(.fields.created)\n'\
+'Updated: \(.fields.updated)\n\n'\
+'\(.fields.description)'\
+'"'
+  query='status!="Closed" AND assignee=currentUser()'
+  selected_issue=$(
+    jira issue list --plain -q "$query" --order-by updatedDate \
+    | sed -e 's/"\(.*\)"/\1/' -e 's/\\t/\t/' \
+    | fzf \
+        --delimiter '\t' \
+        --header-lines 1 \
+        --preview "jira issue view {2} --plain" \
+        --preview-window=top:wrap \
+    | cut -f1
+  )
+  
+  branch_name=$(
+    echo "$selected_issue" |
+    sed -e 's/\. /\t/' -e 's/[^a-zA-Z0-9\t]/-/g' |
+    awk '{printf "%s/%s", $1, tolower($2)}'
+  )
+
+  if [ -n "$branch_name" ]; then
+    git checkout -b "$branch_name"
+    echo "git push -u origin \"$branch_name\""
+  fi
+}
