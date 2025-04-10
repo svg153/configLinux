670b794f404288844d0c23d9283bb4b6374ba711
Sergio Valverde <svg153@gmail.com>
2025-04-10 09:15:20 +0000

    feat: add functions and aliases to open modified files in VSCode

--- .bash_aliases.d/code.sh
+++ .bash_aliases.d/code.sh
@@ -1,12 +1,36 @@
 alias c="code ."
 
-codepr(){
+__get_eddited_files_in_current_branch(){
     declare -a files
     # open all the file added and modified in the current branch in vscode
     while IFS= read -r line; do
         files+=("$(git rev-parse --show-toplevel)/${line}")
     done < <(git diff --name-only origin/main...HEAD)
-    code "${files[@]}"    
+    echo "${files[@]}"
+}
+
+alias cprf="codeprfiles"
+alias ccodeprf="codeprfiles"
+codeprfiles(){
+    declare -a files
+    files=( $(__get_eddited_files_in_current_branch) )
+    echo "Files added and modified in the current branch:"
+    echo "----------------------------------------"
+    for file in "${files[@]}"; do
+        echo - "$file"
+    done
+}
+
+alias cpr="codeprfiles"
+codepr(){
+    declare -a files
+    files=( $(__get_eddited_files_in_current_branch) )
+    if [ ${#files[@]} -gt 0 ]; then
+        echo "Opening ${#files[@]} files in vscode"
+        code "${files[@]}"
+    else
+        echo "No files to open."
+    fi
 }
 
 codefolder(){
