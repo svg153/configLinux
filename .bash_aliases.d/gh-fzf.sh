c3705d0a3bcafd4154643350331940745544e68b
Sergio Valverde <svg153@gmail.com>
2025-06-14 13:27:43 +0200

    Set GH_BROWSER to explorer.exe only if running in WSL

--- .bash_aliases.d/gh-fzf.sh
+++ .bash_aliases.d/gh-fzf.sh
@@ -1,3 +1,9 @@
 # SCRIPT_DIR
 
 # source ./gh-fzf/ghf.bash
+
+# Set GH_BROWSER to explorer.exe only if running in WSL
+isWSL=$(uname -a | grep WSL | wc -l)
+if [ $isWSL -gt 0 ]; then
+    export GH_BROWSER=explorer.exe
+fi
